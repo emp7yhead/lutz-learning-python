42c0b9b94f72ae05fc2e6038b44ca84a4552f19a
emp7yhead <artyomkropp@gmail.com>
2022-08-30 23:43:50 +0900

    Add: abstract.py

---
+++ Classes/abstract.py
@@ -0,0 +1,19 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Super(metaclass=ABCMeta):
+    def delegate(self):
+        self.action()
+
+    @abstractmethod
+    def action(self):
+        pass
+
+
+class Sub(Super):
+    def action(self):
+        print('spam!')
+
+
+X = Sub()
+X.delegate()
